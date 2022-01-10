d2c4a51eeec22344e6faee0ea2a5189baec94bcd
nepp-plus <nepp.plus@gmail.com>
2022-01-10 17:19:09 +0900

    회원가입 기능 실제 호출  - 응답 처리 X

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/SignUpActivity.kt
@@ -2,7 +2,11 @@ package com.neppplus.pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.pythonapipractice_20220110.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,27 @@ class SignUpActivity : BaseActivity() {
 
         btnSubmit.setOnClickListener {
 
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+            val inputNick = edtNickname.text.toString()
 
+            apiList.getRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNick
+            ).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
 
         }
 
