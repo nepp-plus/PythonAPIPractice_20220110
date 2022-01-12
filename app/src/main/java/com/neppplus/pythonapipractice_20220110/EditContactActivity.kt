cff453496c8631cc7a2c59260cecbf28216a52f0
nepp-plus <nepp.plus@gmail.com>
2022-01-12 10:41:34 +0900

    연락처 등록 API 호출 시도  - userId 첨부 방법이 없다.

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
@@ -2,10 +2,24 @@ package com.neppplus.pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_contact.*
 
 class EditContactActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_contact)
+
+        btnSave.setOnClickListener {
+
+            val inputName = edtName.text.toString()
+            val inputPhoneNum = edtPhoneNum.text.toString()
+            val inputMemo = edtMemo.text.toString()
+
+            apiList.getRequestAddContact(
+
+            )
+
+        }
+
     }
 }
